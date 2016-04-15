2fa4153ce99e3950c4d9ed229d826cd2529f4a05
Vladislav Pavlov <vladi_terziiski@abv.bg>
2016-04-15 10:59:07 +0300

    homework

---
+++ C#/1.C#Fundamentals/Exam1/Exam1Task1/Exam1Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exam1Task1
+{
+    class Exam1Task1
+    {
+        static void Main()
+        {
+            long A = long.Parse(Console.ReadLine());
+            long B = long.Parse(Console.ReadLine());
+            long C = long.Parse(Console.ReadLine());
+            double arithmetic = ((A + B + C) / 3.0);
+            long Max1 = Math.Max(A, B);
+            long Max2 = Math.Max(Max1, C);
+            long Min1 = Math.Min(A, B);
+            long Min2 = Math.Min(Min1, C);
+            if (Max1 > Max2)
+            {
+                Console.WriteLine(Max1);
+            }
+            else
+            {
+                Console.WriteLine(Max2);
+            }
+            if (Min1 < Min2)
+            {
+                Console.WriteLine(Min1);
+            }
+            else
+            {
+                Console.WriteLine(Min2);
+            }
+            Console.WriteLine("{0:F2}", arithmetic);
+        }
+    }
+}
---
+++ C#/1.C#Fundamentals/Homework4-ConsoleInputAndOutput/Interval/Interval.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interval
+{
+    class Interval
+    {
+        static void Main()
+        {
+            int amount = 0;
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            if (n > m)
+            {
+                for (int kur = (m +1) ; kur < n; kur++)
+                {
+                    if (kur % 5 == 0)
+                    {
+                        amount++;
+                    }
+                }
+            }
+            else
+            {
+                for (int packa = (n + 1); packa < m; packa++)
+                {
+                    if (packa % 5 == 0)
+                    {
+                        amount++;
+                    }
+                }
+            }
+            Console.WriteLine(amount);
+        }
+        }
+}
+
